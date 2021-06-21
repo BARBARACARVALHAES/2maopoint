02336e37a0b483cb59e92bcae284eb2482b613b9
alexis lenoir <alexis59495@gmail.com>
2021-06-21 17:37:00 -0300

    Add some factories

--- spec/factories/item_category_spec.rb
+++ spec/factories/item_category_spec.rb
@@ -1,6 +1,6 @@
 # This will guess the User class
 FactoryBot.define do
   factory :item_category do
-    name { Faker::Name.first_name }
+    name { ItemCategory::ITEM_CATEGORIES.sample }
   end
 end
