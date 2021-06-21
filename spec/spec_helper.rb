4074dad7595439dae94adab949f1691ba5e371d9
alexis lenoir <alexis59495@gmail.com>
2021-06-21 18:54:07 -0300

    Clean db before litting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,11 +27,11 @@ Capybara.default_max_wait_time = 10
 RSpec.configure do |config|
 
   config.before(:suite) do
-    # Litting FactoryBot (create all the factory and test if there is no validation/creation problem)
-    FactoryBot.lint
     # Clean the DB before/after the tests
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
+    # Litting FactoryBot (create all the factory and test if there is no validation/creation problem)
+    FactoryBot.lint
   end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
