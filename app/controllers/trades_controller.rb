eea107fb519cd4eb65a7e4eefde554a4a8e2f911
alexis lenoir <alexis59495@gmail.com>
2021-06-22 17:46:21 -0300

    Redo logic for step controller

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,6 +1,4 @@
 class TradesController < ApplicationController
-  include Wicked::Wizard
-  steps :calendar, :location, :invitation, :confirmation
   before_action :set_trade, only: %i[edit destroy]
 
   def index
@@ -9,40 +7,18 @@ class TradesController < ApplicationController
 
   def new
     @trade = Trade.new
-    @item_categories = ItemCategory.all
+    # We save an instance of the trade in database even if not valid
+    @trade.save!(validate: false)
+    # We redirect to trade_step_path in order to begin the Wizard form
+    redirect_to trade_step_path(@trade, Trade.form_steps.keys.first)
     authorize @trade
-  end
-
-  def create
-    @trade = Trade.new(trade_params)
-    if params[:author] == "Vendedor"
-      @trade.seller_id = current_user.id
-    else
-      @trade.buyer_id = current_user.id
-    end
-    if @trade.save
-      session[:trade_id] = @trade.id
-      redirect_to wizard_path(steps.first, trade_id: @trade.id)
-    else
-      render :new
-    end
-  end
-
-  def show
-    @trade = Trade.find(session[:trade])
-    case step
-    when :location
-      @carrefour_units = CarrefourUnit.all
-    end
-    render_wizard
   end
 
   def edit; end
 
   def update
-    @trade = Trade.find(session[:trade])
+    @trade = Trade.find(params[:id])
     @trade.update(trade_params)
-    render_wizard @trade
   end
 
   def destroy
@@ -57,7 +33,6 @@ class TradesController < ApplicationController
   end
 
   def trade_params
-    params.require(:trade).permit(:item, :item_category_id, :buyer_id, :seller_id, :carrefour_unit_id, :date,
-                                  :buyer_cep, :seller_cep, :receiver_email, :author)
+    params.require(:trade).permit(:carrefour_unit_id, :date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :trades
+  resources :trades do
+    resources :steps, only: [:show, :update], controller: 'steps_controllers/trades_steps'
+  end
+
+  resources :profiles
 end
