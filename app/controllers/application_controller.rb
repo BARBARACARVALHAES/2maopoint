6c1bcc2e3341f121c6e429e9b371c01305fb37d8
patriciachedid <patriciachedid@gmail.com>
2021-06-22 16:04:12 -0300

    ajuste no redirecionamento da homepage e criação da tela de area do usuario - tela 6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include Pundit
+
+  def after_sign_in_path_for(resource)
+    profile_path(resource)
+  end
+
 end
