9c0f15786069104fb8d5bad4d687c03ffabc60e1
Gustavo Borges <gustborges@gmail.com>
2021-06-22 16:44:44 -0300

    initial commit

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,6 @@
 class ProfilesController < ApplicationController
+  def invitations
+    @user = current_user
+    @authored_trades = Trade.where(author: @user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :trades
+  resources :profiles do
+    get 'profiles/:id/invitations', to: 'profiles#invitations'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
