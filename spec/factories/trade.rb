52714e5414e338f54019a43244ab0ef3faa08a8a
alexis lenoir <alexis59495@gmail.com>
2021-06-23 12:56:50 -0300

    Setup features tests

--- spec/factories/trade.rb
+++ spec/factories/trade.rb
@@ -13,5 +13,50 @@ FactoryBot.define do
     seller_cep { Faker::Address.postcode }
     receiver_email { [buyer.email, seller.email].sample }
     author_role { Trade::ROLE.sample }
+
+    trait :trade_infos do
+      author { association(:user) }
+      buyer { nil }
+      seller { nil }
+      carrefour_unit { nil }
+      item_category { nil }
+      date { nil }
+      buyer_accepted { false }
+      seller_accepted { false }
+      item { nil }
+      buyer_cep { nil }
+      seller_cep { nil }
+      receiver_email { nil }
+      author_role { nil }
+      form_step { :infos }
+    end
+
+    trait :trade_location do
+      author { association(:user) }
+      buyer { nil }
+      seller { nil }
+      carrefour_unit { nil }
+      date { nil }
+      buyer_accepted { false }
+      seller_accepted { false }
+      buyer_cep { nil }
+      seller_cep { nil }
+      receiver_email { nil }
+      form_step { :location }
+    end
+
+    trait :trade_invitation do
+      author { association(:user) }
+      buyer { nil }
+      seller { nil }
+      buyer_accepted { false }
+      seller_accepted { false }
+      receiver_email { nil }
+      form_step { :invitation }
+    end
+
+    factory :trade_infos, traits: [:infos]
+    factory :trade_location, traits: [:location]
+    factory :trade_invitation, traits: [:invitation]
   end
 end
--- spec/features/trades_spec.rb
+++ spec/features/trades_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.feature "Trades", type: :feature, js: true do
   let(:user) { create(:user) }
+  let(:trade_location) { create(:trade_location) }
+  let(:trade_invitation) { create(:trade_invitation) }
 
   context 'Trades new' do
     before do
@@ -14,11 +16,30 @@ RSpec.feature "Trades", type: :feature, js: true do
       expect(page).to have_current_path(trade_step_path(trade, 'infos'))
     end
 
-    scenario 'User complete all the form' do
+    scenario 'infos' do
       visit(new_trade_path)
-      trade = Trade.last
-      check('trade_author_role_vendedor')
-      fill('')
+      choose('trade_author_role_vendedor')
+      find('#trade_item_category_id').find(:xpath, 'option[2]').select_option
+      fill_in('trade_item', with: 'Test item')
+      click_on(id: 'avançar')
+      expect(Trade.all.order(updated_at: :desc).first.item).to eq("Test item")
+    end
+
+    scenario 'location' do
+      visit(trade_step_path(trade_location, "location"))
+      fill_in('trade_buyer_cep', with: '22000-111')
+      fill_in('trade_seller_cep', with: '22000-112')
+      find('#trade_carrefour_unit_id').find(:xpath, 'option[2]').select_option
+      click_on(id: 'avançar')
+      expect(Trade.all.order(updated_at: :desc).first.buyer_cep).to eq("22000-111")
+    end
+
+    scenario 'invitation' do
+      visit(trade_step_path(trade_invitation, "invitation"))
+      fill_in('trade_receiver_email', with: 'test@test.com')
+      click_on(id: 'terminar')
+      expect(Trade.all.order(updated_at: :desc).first.receiver_email).to eq("test@test.com")
     end
   end
 end
+
