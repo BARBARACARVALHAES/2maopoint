4ad52e0a3bb4f8f863b6bd91bd1b1e9e290f6004
alexis lenoir <alexis59495@gmail.com>
2021-06-23 17:01:15 -0300

    Add validations tests

--- app/models/carrefour_unit.rb
+++ app/models/carrefour_unit.rb
@@ -1,7 +1,14 @@
 class CarrefourUnit < ApplicationRecord
   has_many :trades
+  validates :address, presence: true
+  validates :name, presence: true
+  validates :suburb, presence: true
+  validates :cep, presence: true
+  validates :city, presence: true
+  geocoded_by :full_adress
+  after_validation :geocode
 
   def full_adress
-    
+    "#{address}, #{cep}, #{suburb}, #{city}"
   end
 end
--- spec/models/item_category_spec.rb
+++ spec/models/item_category_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe ItemCategory, type: :model do
   it { should have_many(:trades) }
+  it { should validate_presence_of(:name) }
 end
