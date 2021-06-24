12c2cb051228bf47f7082d22abad9d6a96b915d6
Gustavo Borges <gustborges@gmail.com>
2021-06-24 19:27:35 -0300

    Profile#invitations checks if current_user needs to be added in the trades (by checking its email)

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,6 +7,13 @@ class ProfilesController < ApplicationController
   def invitations
     @user = current_user
     authorize @user
+    # TODO: passar esta lógica para "after_sign_up"
+    policy_scope(Trade).each do |trade|
+      if trade.receiver_email == @user.email
+        trade.update(receiver_name: @user.first_name) if @user.first_name
+        trade.author_role == "Vendedor" ? trade.update(buyer: @user) : trade.update(seller: @user)
+      end
+    end
     @trades = Trade.where(buyer: @user).or(Trade.where(seller: @user))
   end
 end
