e42de1845f0fb69f5cab6f938d263800abff8421
alexis lenoir <alexis59495@gmail.com>
2021-06-24 20:46:40 -0300

    Correct flatpicker

--- app/javascript/components/flatpick.js
+++ app/javascript/components/flatpick.js
@@ -2,16 +2,18 @@ import flatpickr from "flatpickr";
 
 const initFlatpick = () => {
   const el = document.getElementById("flat_date");
-  flatpickr(el, {
-    enableTime: true,
-    altInput: true,
-    minDate: "today",
-    altFormat: "F j, Y at h:i K",
-    dateFormat: "Y-m-d H:i",
-    defaultHour: 23,
-    defaultMinute: 59,
-    minuteIncrement: 1,
-  });
+  if(document.getElementById("flat_date")) {
+    flatpickr(el, {
+      enableTime: true,
+      altInput: true,
+      minDate: "today",
+      altFormat: "F j, Y at h:i K",
+      dateFormat: "Y-m-d H:i",
+      defaultHour: 23,
+      defaultMinute: 59,
+      minuteIncrement: 1,
+    });
+  }
 };
 
 export { initFlatpick };
