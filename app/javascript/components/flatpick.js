a889a5eb00d87157a1d7b313d396fd53b6189c6e
Gustavo Borges <gustborges@gmail.com>
2021-06-28 15:40:53 -0300

    Changed forms design

--- app/javascript/components/flatpick.js
+++ app/javascript/components/flatpick.js
@@ -1,26 +1,26 @@
 import flatpickr from "flatpickr";
 
 const initFlatpick = () => {
-  if(document.getElementById("flat_datetime")) {
+  if (document.getElementById("flat_datetime")) {
     const el = document.getElementById("flat_datetime");
     flatpickr(el, {
       enableTime: true,
       altInput: true,
       minDate: "today",
       altFormat: "F j, Y at h:i K",
-      dateFormat: "Y-m-d H:i",
+      dateFormat: "d-m-Y H:M",
       defaultHour: 23,
       defaultMinute: 59,
       minuteIncrement: 1,
     });
   }
-  if(document.getElementById("flat_date")) {
+  if (document.getElementById("flat_date")) {
     const el = document.getElementById("flat_date");
     flatpickr(el, {
       enableTime: false,
       altInput: true,
       altFormat: "F j, Y at h:i K",
-      dateFormat: "Y-m-d",
+      dateFormat: "d-m-Y",
     });
   }
 };
