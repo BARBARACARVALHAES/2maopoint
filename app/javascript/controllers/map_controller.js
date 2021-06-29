9901789e20ff9943b641449200a587e3c733f64d
alexis lenoir <alexis59495@gmail.com>
2021-06-28 22:08:03 -0300

    Start stimulus

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,9 +1,10 @@
 import { Controller } from 'stimulus'; 
 
 export default class extends Controller {
-  static targets = [ "select" ]
+  static targets = [ "select", "mapbox" ]
 
   selectUnit() {
     console.log(this.selectTarget.value)
+    console.log(this.mapboxTarget)
   }
 }
