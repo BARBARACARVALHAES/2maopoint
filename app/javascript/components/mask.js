9415903d1ae9fdd13da1ad5c87206784f90aa6dc
patriciachedid <patriciachedid@gmail.com>
2021-07-01 14:12:06 -0300

    tirando o +55 da mascara

--- app/javascript/components/mask.js
+++ app/javascript/components/mask.js
@@ -6,7 +6,7 @@ var initiMask = function () {
   $(".date_time").mask("00/00/0000");
   $(".cep").mask("00000-000");
   $(".phone").mask("(00) (00000-0000");
-  $(".phone_with_ddd").mask("+55 (00) 00000-0000");
+  $(".phone_with_ddd").mask("(00) 00000-0000");
   $(".phone_us").mask("(000) 000-0000");
   $(".mixed").mask("AAA 000-S0S");
   $(".cpf").mask("000.000.000-00", { reverse: true });
